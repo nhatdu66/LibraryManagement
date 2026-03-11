7cc04ab3f0d011d71aebddc9fb6d3bdad4f5ab41
Giann Lương Sơn <glson0318@gmail.com>
2026-03-11 12:54:12 +0700

    update feature

---
+++ BusinessObjects/Category.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObjects
+{
+    public partial class Category
+    {
+        public Category() 
+        { 
+            Products = new HashSet<Product>();
+        }
+        public Category(int catId, string catName)
+        {
+            this.CategoryId = catId;
+            this.CategoryName = catName;
+        }
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public virtual ICollection<Product> Products { get; set; }
+    }
+}
---
+++ Repositories/ProductRepository.cs
@@ -0,0 +1,24 @@
+﻿using BusinessObjects;
+using DataAccessObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ProductDAO _productDAO;
+        public ProductRepository()
+        {
+            _productDAO = new ProductDAO();
+        }
+        public void DeleteProduct(Product p) => _productDAO.DeleteProduct(p);
+        public void SaveProduct(Product p) => _productDAO.SaveProduct(p);
+        public void UpdateProduct(Product p) => _productDAO.UpdateProduct(p);
+        public List<Product> GetProducts() => _productDAO.GetProducts();
+        public Product GetProductById(int id) => _productDAO.GetProductById(id);
+    }
+}
